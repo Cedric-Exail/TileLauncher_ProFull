fb9611e9cf3113f30294d9391dec04794943599f
Cedric-Exail <cedric.blin.exail@gmail.com>
2026-05-19 17:56:20 +0200

    Refactor Logger methods for clarity and consistency

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,8 +1,41 @@
 #include "Logger.h"
 #include <QDateTime>
-Logger& Logger::instance(){ static Logger i; return i; }
-void Logger::open(const QString& log,const QString& dat){ file.setFileName(log); file.open(QIODevice::Append|QIODevice::WriteOnly); datPath=dat; }
-void Logger::log(const QString& m){ file.write((QString("[")+QDateTime::currentDateTime().toString()+"] "+m+"
-").toUtf8()); file.flush(); }
-void Logger::start(){ timer.start(); m_data.launchCount++; log("START"); }
-void Logger::stop(){ long long s=timer.elapsed()/1000; m_data.usageSeconds+=s; log(QString("STOP %1s").arg(s)); m_data.save(datPath); }
+
+Logger& Logger::instance()
+{
+    static Logger inst;
+    return inst;
+}
+
+void Logger::open(const QString& log, const QString& dat)
+{
+    file.setFileName(log);
+    file.open(QIODevice::Append | QIODevice::WriteOnly);
+    datPath = dat;
+}
+
+void Logger::log(const QString& message)
+{
+    QString line = QString("[%1] %2\n")
+        .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss"))
+        .arg(message);
+
+    file.write(line.toUtf8());
+    file.flush();
+}
+
+void Logger::start()
+{
+    timer.start();
+    m_data.launchCount++;
+    log("START");
+}
+
+void Logger::stop()
+{
+    qint64 secs = timer.elapsed() / 1000;
+    m_data.usageSeconds += secs;
+
+    log(QString("STOP %1 seconds").arg(secs));
+    m_data.save(datPath);
+}
